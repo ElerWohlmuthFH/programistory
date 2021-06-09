e6bf39edef93062b64290617ede7526bb5da3921
ElerWohlmuthFH <eler.wohlmuth@stud.fh-campuswien.ac.at>
2021-06-10 00:33:11 +0200

    bugfixes

--- programistory/main.js
+++ programistory/main.js
@@ -1,55 +1,62 @@
-// function loadDoc() {
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             document.getElementById("demo").innerHTML =
-//                 this.responseText;
-//         }
-//     };
-//     xhttp.open("GET", "main.xml", true);
-//     xhttp.send();
-// }
+//AJAX - LOAD XML FILES
 
+function load_adalovelace_page_1() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("al").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "adalovelace_page_1.xml", true);
+    xhttp.send();
+}
 
-// Get the modal
+function load_adalovelace_page_2() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("al").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "adalovelace_page_2.xml", true);
+    xhttp.send();
+}
+
+//POP-UP BOX:
+
+// Get the box
 var box1 = document.getElementById("adaLovelace_content");
 var box2 = document.getElementById("graceHopper_content");
 
-// Get the button that opens the modal
+// Get the button that opens the box
 var btn = document.getElementById("adaLovelace");
 var btn2 = document.getElementById("graceHopper");
 
 
-
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the box
 var span = document.getElementsByClassName("close")[0];
 
 
-
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
+// When the user clicks the button, open the box
+btn.onclick = function () {
     box1.style.display = "block";
 }
 
-btn2.onclick = function() {
+btn2.onclick = function () {
     box2.style.display = "block";
 }
 
 
-
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+// When the user clicks on <span> (x), close the box
+span.onclick = function () {
     box1.style.display = "none";
 }
 
 
-
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == box1) {
         box1.style.display = "none";
     }
